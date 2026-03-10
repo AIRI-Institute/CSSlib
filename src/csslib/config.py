64e42465c876a1a5751d9d52dee874d4f04504a3
asolovykh <solovykhaa@my.msu.ru>
2026-03-10 12:11:18 +0300

    Docstrings in config.py are changed

--- src/csslib/config.py
+++ src/csslib/config.py
@@ -6,18 +6,18 @@ from typing import Optional
 
 class Substitutions(BaseModel):
     '''
-        Substitution field pydantic scheme class.
+        Substitution field pydantic scheme class of the config.
         
-        Fields:
-        - "specie_to_substitute" - string with a chemical element which should be replaced (mandatory)
-        - "substitute_with" - string with with a chemical element that will replace the original chemical element (mandatory)
-        - "substitution_low_limit" - float value containing lower boundary of the substitution (mandatory). Example: 0.05
-        - "substitution_high_limit" - float value containing higher boundary of the substitution (mandatory). Example: 0.5
+        Mandatory fields:
+          specie_to_substitute (str): the chemical element that must be replaced
+          substitute_with (str): the chemical element that will replace the original chemical element
+          substitution_low_limit (float): lower boundary of the substitution. Restrictions: >= 0 and < 1. Example: 0.05
+          substitution_high_limit (float): higher boundary of the substitution. Restrictions: > 0, <= 1 and > substitution_low_limit. Example: 0.5
         
-        Internal parameters:
-        - "substitution_low_limit_natoms"
-        - "substitution_high_limit_natoms"
-        - "indices_to_substitute"
+        Fields that must be empty:
+          substitution_low_limit_natoms (int): the minimum number of atoms that must be replaced 
+          substitution_high_limit_natoms (int): the maximum number of atoms that must be replaced
+          indices_to_substitute (list[int]): the list of atom indices that must be replaced
     '''
     model_config = ConfigDict(extra="forbid")
     specie_to_substitute: str
@@ -33,12 +33,14 @@ class Config(BaseModel):
     '''
         Config pydantic scheme class.
         
-        Fields:
-        - "result_dir" - full or relative path to the results directory (mandatory)
-        - "structure_filename" - full or relative path to the .cif initial structure file (mandatory)
-        - "supercell" - system replication numbers in the following format: "2x1x1", "2x2x1", ... (optional, default value - "1x1x1")
-        - "num_workers" - number of parallel processes (optional, default value - 1)
-        - "substitution" - list of required substitutions for the system (optional)
+        Mandatory fields:
+          result_dir (str): a full or relative path to the results directory
+          structure_filename (str): a full or relative path to the .cif initial structure file
+        
+        Optional fields:
+          supercell (str): system replication numbers in the following format: "2x1x1", "2x2x1", ... (default value - "1x1x1")
+          num_workers (int): the number of parallel processes (default value - 1)
+          substitution (list[Substitutions]): list of required substitutions for the system
     '''
     model_config = ConfigDict(extra="forbid")
     result_dir: str
@@ -51,6 +53,12 @@ class Config(BaseModel):
 def get_available_config_fields(output=sys.stdout):
     '''
         Prints all possible config field names.
+        
+        Args:
+          output (TextIO | Any): an output stream
+        
+        Returns:
+          None
     '''
     message = 'List of available config fields:\n'
     message += '  - "result_dir" - full or relative path to the results directory (mandatory)\n'
@@ -73,6 +81,12 @@ def get_available_config_fields(output=sys.stdout):
 def get_example_config(output=sys.stdout):
     '''
         Prints example config.
+        
+        Args:
+          output (TextIO | Any): an output stream
+        
+        Returns:
+          None
     '''
     message = '''{
   "result_dir": "path-to-directory-with-results",
